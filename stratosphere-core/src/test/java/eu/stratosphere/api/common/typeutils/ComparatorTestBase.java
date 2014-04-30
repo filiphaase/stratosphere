30519bac817d6bae62f78176b9c563c9fd4b3d28
zentol <s.motsu@web.de>
2014-04-30 19:11:08 +0200

    Test fo Greater/Smaller with Asc/Desc

--- stratosphere-core/src/test/java/eu/stratosphere/api/common/typeutils/ComparatorTestBase.java
+++ stratosphere-core/src/test/java/eu/stratosphere/api/common/typeutils/ComparatorTestBase.java
@@ -14,89 +14,143 @@
  **********************************************************************************************************************/
 package eu.stratosphere.api.common.typeutils;
 
-import static org.junit.Assert.*;
-
+import eu.stratosphere.core.memory.DataInputView;
+import eu.stratosphere.core.memory.DataOutputView;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-
+import java.util.Arrays;
+import static org.junit.Assert.*;
 import org.junit.Test;
-
-import eu.stratosphere.api.common.typeutils.TypeSerializer;
-import eu.stratosphere.core.memory.DataInputView;
-import eu.stratosphere.core.memory.DataOutputView;
 
 /**
  * Abstract test base for serializers.
  */
 public abstract class ComparatorTestBase<T> {
-	
+
 	protected abstract TypeComparator<T> createComparator(boolean ascending);
-	
+
 	protected abstract TypeSerializer<T> createSerializer();
-	
+
 	protected abstract int getNormalizedKeyLength();
-	
+
 	protected abstract Class<T> getTypeClass();
-	
+
 	protected abstract T[] getSortedTestData();
 
 	// --------------------------------------------------------------------------------------------
-	
-
 	@Test
 	public void testGetLength() {
 		try {
 			TypeComparator<T> comparator = getComparator(true);
 			assertEquals(getNormalizedKeyLength(), comparator.getNormalizeKeyLen());
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 			e.printStackTrace();
 			fail("Exception in test: " + e.getMessage());
 		}
 	}
-	
+
 	@Test
 	public void testEqualsAscending() {
 		testEquals(true);
 	}
-	
+
 	@Test
 	public void testEqualsDescending() {
 		testEquals(false);
 	}
-	
+
 	private void testEquals(boolean ascending) {
 		try {
 			// Just setup two identical output/inputViews and go over their data to see if compare works
 			TestOutputView out1 = new TestOutputView();
 			writeSortedData(getSortedData(), out1);
-			
+
 			TestOutputView out2 = new TestOutputView();
 			writeSortedData(getSortedData(), out2);
-			
+
 			TestInputView in1 = out1.getInputView();
 			TestInputView in2 = out2.getInputView();
 
 			// Now use comparator and compar
 			TypeComparator<T> comparator = getComparator(ascending);
 			T[] data = getSortedData();
-			for(T e : data){
+			for (T e : data) {
 				assertTrue(comparator.compare(in1, in2) == 0);
 			}
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			System.err.println(e.getMessage());
 			e.printStackTrace();
 			fail("Exception in test: " + e.getMessage());
 		}
 	}
-	
+
+	@Test
+	public void testGreaterAscending() {
+		test(true, true);
+	}
+
+	@Test
+	public void testGreaterDescending() {
+		test(false, true);
+	}
+
+	@Test
+	public void testSmallerAscending() {
+		test(true, false);
+	}
+
+	@Test
+	public void testSmallerDescending() {
+		test(false, false);
+	}
+
+	public void test(boolean ascending, boolean greater) {
+		try {
+			// Now use comparator and compar
+			T[] data = getSortedData();
+			T[] low = Arrays.copyOfRange(data, 0, data.length / 2);
+			T[] high = Arrays.copyOfRange(data, data.length / 2, data.length);
+
+			TypeComparator<T> comparator = getComparator(ascending);
+			TestOutputView out1;
+			TestOutputView out2;
+			TestInputView in1;
+			TestInputView in2;
+
+			//compares every element in high with every element in low
+			for (T h : high) {
+				//workaround for generic array creation
+				T[] selectedH = Arrays.copyOf(low, 0);
+				for (int x = 0; x < selectedH.length; x++) {
+					selectedH[x] = h;
+				}
+				out2 = new TestOutputView();
+				writeSortedData(selectedH, out2);
+				in2 = out2.getInputView();
+
+				out1 = new TestOutputView();
+				writeSortedData(low, out1);
+				in1 = out1.getInputView();
+				for (T l : low) {
+					if (greater) {
+						assertTrue(comparator.compare(in1, in2) < 0);
+					} else {
+						assertTrue(comparator.compare(in1, in2) > 0);
+					}
+				}
+			}
+		} catch (Exception e) {
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+			fail("Exception in test: " + e.getMessage());
+		}
+	}
+
 	// --------------------------------------------------------------------------------------------
-	
 	protected void deepEquals(String message, T should, T is) {
 		if (should.getClass().isArray()) {
 			if (should instanceof long[]) {
@@ -105,12 +159,11 @@ public abstract class ComparatorTestBase<T> {
 				assertArrayEquals(message, (Object[]) should, (Object[]) is);
 			}
 		} else {
-			assertEquals(message,  should, is);
+			assertEquals(message, should, is);
 		}
 	}
-	
-	// --------------------------------------------------------------------------------------------
 
+	// --------------------------------------------------------------------------------------------
 	private TypeComparator<T> getComparator(boolean ascending) {
 		TypeComparator<T> comparator = createComparator(ascending);
 		if (comparator == null) {
@@ -118,7 +171,7 @@ public abstract class ComparatorTestBase<T> {
 		}
 		return comparator;
 	}
-	
+
 	private T[] getSortedData() {
 		T[] data = getSortedTestData();
 		if (data == null) {
@@ -126,7 +179,7 @@ public abstract class ComparatorTestBase<T> {
 		}
 		return data;
 	}
-	
+
 	private TypeSerializer<T> getSerializer() {
 		TypeSerializer<T> serializer = createSerializer();
 		if (serializer == null) {
@@ -134,34 +187,33 @@ public abstract class ComparatorTestBase<T> {
 		}
 		return serializer;
 	}
-	
-	private void writeSortedData(T[] data, TestOutputView out) throws IOException{
+
+	private void writeSortedData(T[] data, TestOutputView out) throws IOException {
 		TypeSerializer<T> serializer = getSerializer();
-		
+
 		// Write data into a outputView
 		for (T value : data) {
-			serializer.serialize(value, out); 
+			serializer.serialize(value, out);
 		}
-		
+
 		// This are the same tests like in the serializer
 		// Just look if the data is really there after serialization, before testing comparator on it
 		TestInputView in = out.getInputView();
-		for(T value: data){
+		for (T value : data) {
 			assertTrue("No data available during deserialization.", in.available() > 0);
-			
+
 			T deserialized = serializer.deserialize(serializer.createInstance(), in);
 			deepEquals("Deserialized value if wrong.", value, deserialized);
 		}
 	}
-	
+
 	// --------------------------------------------------------------------------------------------
-	
 	private static final class TestOutputView extends DataOutputStream implements DataOutputView {
-		
+
 		public TestOutputView() {
 			super(new ByteArrayOutputStream(4096));
 		}
-		
+
 		public TestInputView getInputView() {
 			ByteArrayOutputStream baos = (ByteArrayOutputStream) out;
 			return new TestInputView(baos.toByteArray());
@@ -181,8 +233,7 @@ public abstract class ComparatorTestBase<T> {
 			write(buffer);
 		}
 	}
-	
-	
+
 	private static final class TestInputView extends DataInputStream implements DataInputView {
 
 		public TestInputView(byte[] data) {
